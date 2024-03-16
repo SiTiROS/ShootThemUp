98ffe509b8deefe63a79e8c4431f26802af5b3b3
SiTiROS <sitiros666@gmail.com>
2024-03-17 05:34:17 +0600

    Lesson 58

---
+++ Source/ShootThemUp/Private/Animations/STUEquipFinishedAnimNotify.cpp
@@ -0,0 +1,9 @@
+// ShootThemUp Game, All Rights Reserved.
+
+#include "Animations/STUEquipFinishedAnimNotify.h"
+
+void USTUEquipFinishedAnimNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+    OnNotified.Broadcast(MeshComp);
+    Super::Notify(MeshComp, Animation);
+}
