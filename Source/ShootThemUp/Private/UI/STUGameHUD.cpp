afddf4a2f36bcd3bc069ec6a8e4d015b5a8a37da
SiTiROS <sitiros666@gmail.com>
2024-04-02 12:59:49 +0600

    Lesson 68

--- Source/ShootThemUp/Private/UI/STUGameHUD.cpp
+++ Source/ShootThemUp/Private/UI/STUGameHUD.cpp
@@ -1,13 +1,23 @@
 // ShootThemUp Game, All Rights Reserved.
 
-
 #include "UI/STUGameHUD.h"
 #include "Engine/Canvas.h"
+#include "Blueprint/UserWidget.h"
 
 void ASTUGameHUD::DrawHUD() 
 {
     Super::DrawHUD();
     DrawCrossHair();
+}
+
+void ASTUGameHUD::BeginPlay()
+{
+    Super::BeginPlay();
+    auto PlayerHUDWidget = CreateWidget<UUserWidget>(GetWorld(), PlayerHUDWidgetClass);
+    if (PlayerHUDWidget)
+    {
+        PlayerHUDWidget->AddToViewport();
+    }
 }
 
 void ASTUGameHUD::DrawCrossHair()
