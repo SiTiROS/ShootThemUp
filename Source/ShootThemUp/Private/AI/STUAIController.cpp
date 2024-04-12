b212b3a8e883771252daf069ceff834223968133
SiTiROS <sitiros666@gmail.com>
2024-04-12 19:19:30 +0600

    Lesson 96

--- Source/ShootThemUp/Private/AI/STUAIController.cpp
+++ Source/ShootThemUp/Private/AI/STUAIController.cpp
@@ -1,3 +1,15 @@
 // ShootThemUp Game, All Rights Reserved.
 
 #include "AI/STUAIController.h"
+#include "AI/STUAICharacter.h"
+
+void ASTUAIController::OnPossess(APawn* InPawn)
+{
+    Super::OnPossess(InPawn);
+
+    const auto STUCharacter = Cast<ASTUAICharacter>(InPawn);
+    if (STUCharacter)
+    {
+        RunBehaviorTree(STUCharacter->BehaviorTreeAsset);
+    }
+}
