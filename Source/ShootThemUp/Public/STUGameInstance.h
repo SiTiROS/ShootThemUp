e2381acec9e685d6eee96b70bd8a124a1b5e1dae
SiTiROS <sitiros666@gmail.com>
2024-04-18 23:49:47 +0600

    Lesson 132

--- Source/ShootThemUp/Public/STUGameInstance.h
+++ Source/ShootThemUp/Public/STUGameInstance.h
@@ -10,4 +10,11 @@ UCLASS()
 class SHOOTTHEMUP_API USTUGameInstance : public UGameInstance
 {
     GENERATED_BODY()
+
+public:
+    FName GetStartupLevelName() const { return StartupLevelName; }
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "UI")
+    FName StartupLevelName = NAME_None;
 };
